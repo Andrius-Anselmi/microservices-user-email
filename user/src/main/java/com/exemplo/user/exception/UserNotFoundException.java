ee96773e402beb3c00e78897697e4049c3136d89
andrius anselmi <andriusanselmi12@gmail.com>
2026-07-10 15:47:09 -0300

    feat: implement update, get by id and delete user flows
    - Add updateById method to UserService
    - Add getUserById handling with UserNotFoundException
    - Add deleteById handling with UserNotFoundException
    - Add PUT, GET by id and DELETE endpoints to UserController
    - Keep email uniqueness validation on save and update

---
+++ user/src/main/java/com/exemplo/user/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.exemplo.user.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String id) {
+        super("User with id '" + id + "' was not found.");
+    }
+}
+
